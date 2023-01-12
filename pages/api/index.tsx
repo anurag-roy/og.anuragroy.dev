ac2c018f4c92590874efa32469dc555d5441730b
Anurag Roy <anuragroy@duck.com>
2023-01-12 12:07:57 +0530

    Use `fluentFlat` due to twemoji outage

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -94,6 +94,7 @@ export default async function handler(req: NextRequest) {
     {
       width: 1200,
       height: 630,
+      emoji: 'fluentFlat',
       fonts: [
         {
           name: 'Satoshi',
