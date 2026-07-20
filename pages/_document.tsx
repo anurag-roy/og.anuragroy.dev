02452be0a2aa5fa97c93909bc6b8a01aa7e43b0e
Anurag Roy <anuragroy@duck.com>
2026-07-21 04:14:43 +0530

    Apply Geist font to portalled UI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
+import { geist } from '@/lib/fonts';
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-h-screen bg-background antialiased">
+      <body
+        className={`${geist.variable} min-h-screen bg-background font-sans antialiased`}
+      >
         <Main />
         <NextScript />
       </body>
