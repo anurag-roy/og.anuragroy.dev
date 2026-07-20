0ca62978b7e08917cb0dde312cf3bcd76a2ff423
Anurag Roy <anuragroy@duck.com>
2026-07-21 04:14:33 +0530

    Use shared Geist font configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import type { AppProps } from 'next/app';
-import { Geist } from 'next/font/google';
 import '../styles/globals.css';
-
-const geist = Geist({
-  subsets: ['latin'],
-  variable: '--font-geist-sans',
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${geist.variable} min-h-svh font-sans`}>
+    <div className="min-h-svh">
       <Component {...pageProps} />
     </div>
   );
