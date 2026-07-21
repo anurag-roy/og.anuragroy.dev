3e2c58567cf6c10a78fde7b4d5d776b20a6cbb85
Anurag Roy <anuragroy@duck.com>
2026-07-21 13:20:10 +0530

    Apply Geist from the custom App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
+import { geist } from '@/lib/fonts';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-svh">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className={`${geist.className} min-h-svh`}>
+        <Component {...pageProps} />
+      </div>
+      <style jsx global>{`
+        :root {
+          --font-geist-sans: ${geist.style.fontFamily};
+        }
+      `}</style>
+    </>
   );
 }
