e8a84281ed36bf51995e9c2e98576eebb8e12e40
Anurag Roy <anuragroy@duck.com>
2026-07-21 13:19:58 +0530

    Keep font loading out of custom Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,10 @@
-import { geist } from '@/lib/fonts';
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body
-        className={`${geist.variable} min-h-screen bg-background font-sans antialiased`}
-      >
+      <body className="min-h-screen bg-background antialiased">
         <Main />
         <NextScript />
       </body>
